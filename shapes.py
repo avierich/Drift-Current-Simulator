a3d873f3ad131d47aeae5904fc5a64ec434704bb
Alex Vierich <avierich@gmail.com>
2017-01-19 16:25:49 -0500

    Converted tabs to spaces

--- shapes.py
+++ shapes.py
@@ -1,127 +1,127 @@
 import numpy as np
 
 def generateArrays(edges, polys) :
-	wallPosTemp = []
-	wallVecTemp = []
-	for edge in edges :
-		wallPosTemp.append([edge[0:2]])
-		wallVecTemp.append([edge[2:4]])
-	wallPos = np.array(wallPosTemp)
-	wallVec = np.array(wallVecTemp)
+    wallPosTemp = []
+    wallVecTemp = []
+    for edge in edges :
+        wallPosTemp.append([edge[0:2]])
+        wallVecTemp.append([edge[2:4]])
+    wallPos = np.array(wallPosTemp)
+    wallVec = np.array(wallVecTemp)
 
-	wallNormTemp = []
-	for wall in wallVec :
-		length = np.sqrt(wall[0][0]**2 + wall[0][1]**2)
-		x = wall[0][1] / length
-		y = -1.0 * wall[0][0] / length
-		wallNormTemp.append([[x,y]])
+    wallNormTemp = []
+    for wall in wallVec :
+        length = np.sqrt(wall[0][0]**2 + wall[0][1]**2)
+        x = wall[0][1] / length
+        y = -1.0 * wall[0][0] / length
+        wallNormTemp.append([[x,y]])
 
-	wallNorm = np.array(wallNormTemp)
-	return wallPos, wallVec, wallNorm, polys, edges
+    wallNorm = np.array(wallNormTemp)
+    return wallPos, wallVec, wallNorm, polys, edges
 
 def trivialExample(width, height) :
 
-	edges = [[0.0,height,width,0.0],[width,0.0,-width,0.0]]
+    edges = [[0.0,height,width,0.0],[width,0.0,-width,0.0]]
 
-	polys = []
+    polys = []
 
-	return generateArrays(edges, polys)
+    return generateArrays(edges, polys)
 
 
 def trapazoidBottleNeck(width, height) :
 
-	edges = [[0.0,height,width/3,0.0],
-		[width/3,height,0.0,-height/4],
-		[width/3,3*height/4,width/9,-height/8],
-		[4*width/9,5*height/8,width/9,0.0],
-		[5*width/9,5*height/8,width/9,height/8],
-		[6*width/9,6*height/8,0.0,height/4],
-		[6*width/9,height,width/3,0.0],
-		[width,0.0,-width/3,0.0],
-		[2*width/3,0.0,0.0,height/4],
-		[2*width/3,height/4,-width/9,height/8],
-		[5*width/9,3*height/8,-width/9,0.0],
-		[4*width/9,3*height/8,-width/9,-height/8],
-		[3*width/9,2*height/8,0.0,-height/4],
-		[3*width/9,0.0,-width/3,0.0]]
+    edges = [[0.0,height,width/3,0.0],
+        [width/3,height,0.0,-height/4],
+        [width/3,3*height/4,width/9,-height/8],
+        [4*width/9,5*height/8,width/9,0.0],
+        [5*width/9,5*height/8,width/9,height/8],
+        [6*width/9,6*height/8,0.0,height/4],
+        [6*width/9,height,width/3,0.0],
+        [width,0.0,-width/3,0.0],
+        [2*width/3,0.0,0.0,height/4],
+        [2*width/3,height/4,-width/9,height/8],
+        [5*width/9,3*height/8,-width/9,0.0],
+        [4*width/9,3*height/8,-width/9,-height/8],
+        [3*width/9,2*height/8,0.0,-height/4],
+        [3*width/9,0.0,-width/3,0.0]]
 
-	polys = [[[width/3,height,0.0,-height/4],
-		[width/3,3*height/4,width/9,-height/8],
-		[4*width/9,5*height/8,width/9,0.0],
-		[5*width/9,5*height/8,width/9,height/8],
-		[6*width/9,6*height/8,0.0,height/4]],
-		[[2*width/3,0.0,0.0,height/4],
-		[2*width/3,height/4,-width/9,height/8],
-		[5*width/9,3*height/8,-width/9,0.0],
-		[4*width/9,3*height/8,-width/9,-height/8],
-		[3*width/9,2*height/8,0.0,-height/4]]]
+    polys = [[[width/3,height,0.0,-height/4],
+        [width/3,3*height/4,width/9,-height/8],
+        [4*width/9,5*height/8,width/9,0.0],
+        [5*width/9,5*height/8,width/9,height/8],
+        [6*width/9,6*height/8,0.0,height/4]],
+        [[2*width/3,0.0,0.0,height/4],
+        [2*width/3,height/4,-width/9,height/8],
+        [5*width/9,3*height/8,-width/9,0.0],
+        [4*width/9,3*height/8,-width/9,-height/8],
+        [3*width/9,2*height/8,0.0,-height/4]]]
 
-	return generateArrays(edges, polys)
+    return generateArrays(edges, polys)
 
 def squareBottleNeck(width, height) :
 
-	edges = [[0.0,height,2*width/5,0.0],
-		[2*width/5,height,0.0,-2*height/5],
-		[2*width/5,3*height/5,width/5,0],
-		[3*width/5,3*height/5,0,2*height/5],
-		[3*width/5,height,2*width/5,0],
-		[width,0.0,-2*width/5,0],
-		[3*width/5,0.0,0.0,2*height/5],
-		[3*width/5,2*height/5,-width/5,0.0],
-		[2*width/5,2*height/5,0.0,-2*height/5],
-		[2*width/5,0.0,-2*width/5,0.0]]
-			
+    edges = [[0.0,height,2*width/5,0.0],
+        [2*width/5,height,0.0,-2*height/5],
+        [2*width/5,3*height/5,width/5,0],
+        [3*width/5,3*height/5,0,2*height/5],
+        [3*width/5,height,2*width/5,0],
+        [width,0.0,-2*width/5,0],
+        [3*width/5,0.0,0.0,2*height/5],
+        [3*width/5,2*height/5,-width/5,0.0],
+        [2*width/5,2*height/5,0.0,-2*height/5],
+        [2*width/5,0.0,-2*width/5,0.0]]
+            
 
-	polys = [[[2*width/5,height,0.0,-2*height/5],
-		[2*width/5,3*height/5,width/5,0],
-		[3*width/5,3*height/5,0,2*height/5]],
-		[[3*width/5,0.0,0.0,2*height/2],
-		[3*width/5,2*height/5,-width/5,0.0],
-		[2*width/5,2*height/5,0.0,-2*height/5]]]
+    polys = [[[2*width/5,height,0.0,-2*height/5],
+        [2*width/5,3*height/5,width/5,0],
+        [3*width/5,3*height/5,0,2*height/5]],
+        [[3*width/5,0.0,0.0,2*height/2],
+        [3*width/5,2*height/5,-width/5,0.0],
+        [2*width/5,2*height/5,0.0,-2*height/5]]]
 
-	return generateArrays(edges, polys)
+    return generateArrays(edges, polys)
 
 def parabolicFocus(width, height) :
 
-	edges = []
-	polys = []
+    edges = []
+    polys = []
 
-	f = lambda x: 1.0/(2.0*height)*(x-width/2.0)**2
+    f = lambda x: 1.0/(2.0*height)*(x-width/2.0)**2
 
-	numSegments = 20
+    numSegments = 20
 
-	edges.append([0.0,height/2,0.0,-height/2])
-	for i in range(numSegments,0,-1) :
-		x1 = float(i)*width/float(numSegments)
-		x2 = float(i-1)*width/float(numSegments)
-		y1 = f(x1)
-		y2 = f(x2)
-		edges.append([x1,y1,x2-x1,y2-y1])
-	edges.append([0.0,0.0,width,0.0])
-	edges.append([width,0.0,0.0,height/2])
+    edges.append([0.0,height/2,0.0,-height/2])
+    for i in range(numSegments,0,-1) :
+        x1 = float(i)*width/float(numSegments)
+        x2 = float(i-1)*width/float(numSegments)
+        y1 = f(x1)
+        y2 = f(x2)
+        edges.append([x1,y1,x2-x1,y2-y1])
+    edges.append([0.0,0.0,width,0.0])
+    edges.append([width,0.0,0.0,height/2])
 
-	polys.append(edges)
+    polys.append(edges)
 
-	g = lambda x: -1.0/(2.0*height)*(x-width/2.0)**2 + height
-	
-	secondPoly = []
-	for i in range(numSegments) :
-		x1 = float(i)*width/float(numSegments)
-		x2 = float(i+1)*width/float(numSegments)
-		y1 = g(x1)
-		y2 = g(x2)
-		edges.append([x1,y1,x2-x1,y2-y1])
-		secondPoly.append([x1,y1,x2-x1,y2-y1])
-	edges.append([width,height/2,0.0,height/2])
-	edges.append([width,height,-width,0.0])
-	edges.append([0.0,height,0.0,-height/2])
-	secondPoly.append([width,height/2,0.0,height/2])
-	secondPoly.append([width,height,-width,0.0])
-	secondPoly.append([0.0,height,0.0,-height/2])
+    g = lambda x: -1.0/(2.0*height)*(x-width/2.0)**2 + height
+    
+    secondPoly = []
+    for i in range(numSegments) :
+        x1 = float(i)*width/float(numSegments)
+        x2 = float(i+1)*width/float(numSegments)
+        y1 = g(x1)
+        y2 = g(x2)
+        edges.append([x1,y1,x2-x1,y2-y1])
+        secondPoly.append([x1,y1,x2-x1,y2-y1])
+    edges.append([width,height/2,0.0,height/2])
+    edges.append([width,height,-width,0.0])
+    edges.append([0.0,height,0.0,-height/2])
+    secondPoly.append([width,height/2,0.0,height/2])
+    secondPoly.append([width,height,-width,0.0])
+    secondPoly.append([0.0,height,0.0,-height/2])
 
-	polys.append(secondPoly)
+    polys.append(secondPoly)
 
-	return generateArrays(edges, polys)
+    return generateArrays(edges, polys)
 
-		
+        
 
--- sim.py
+++ sim.py
@@ -7,152 +7,152 @@ from matplotlib.animation import FuncAnimation
 MAX_PLOT_PATHS = 10
 
 def initElectrons(numElectrons, width, height, vtherm, polygons) :
-	tempPosition = []
-	tempVelocity = []
-	paths = []
+    tempPosition = []
+    tempVelocity = []
+    paths = []
 
-	for i in range(numElectrons) :
-		# Generate random position
-		valid = False
-		while not valid :
-			attemptPos = np.array([np.random.uniform(0, width),
-						np.random.uniform(0, height)])
-			valid = not isPointInPolys(polygons, attemptPos)
-			if valid :
-				tempPosition.append([[attemptPos[0],
-							attemptPos[1]]])
-		
+    for i in range(numElectrons) :
+        # Generate random position
+        valid = False
+        while not valid :
+            attemptPos = np.array([np.random.uniform(0, width),
+                        np.random.uniform(0, height)])
+            valid = not isPointInPolys(polygons, attemptPos)
+            if valid :
+                tempPosition.append([[attemptPos[0],
+                            attemptPos[1]]])
+        
 
-		angle = np.random.uniform(0,2*np.pi)
-		tempVelocity.append([[vtherm*np.cos(angle),
-				vtherm*np.sin(angle)]])
-#		tempVelocity.append([[0,
-#				vtherm*np.sin(angle)]])
+        angle = np.random.uniform(0,2*np.pi)
+        tempVelocity.append([[vtherm*np.cos(angle),
+                vtherm*np.sin(angle)]])
+#       tempVelocity.append([[0,
+#               vtherm*np.sin(angle)]])
 
 
-	for i in range(min(numElectrons,MAX_PLOT_PATHS)) :
-		paths.append([[],[]])
+    for i in range(min(numElectrons,MAX_PLOT_PATHS)) :
+        paths.append([[],[]])
 
-	return np.array(tempPosition), np.array(tempVelocity), paths
+    return np.array(tempPosition), np.array(tempVelocity), paths
 
 # This function checks if a given point lies in any of the given polygons
 def isPointInPolys(polygons,point):
-	for polygon in polygons :
-		numIntersections = 0
-		for edge in polygon :
-			wallPos = np.array([edge[0], edge[1]])
-			wallVec = np.array([edge[2], edge[3]])
-			pointPos = np.array([-100e-9,-100e-9])
-			pointVec = np.array([point[0] + 100e-9, point[1] +100e-9])
+    for polygon in polygons :
+        numIntersections = 0
+        for edge in polygon :
+            wallPos = np.array([edge[0], edge[1]])
+            wallVec = np.array([edge[2], edge[3]])
+            pointPos = np.array([-100e-9,-100e-9])
+            pointVec = np.array([point[0] + 100e-9, point[1] +100e-9])
 
-			t = np.cross(wallPos - pointPos, wallVec) / np.cross(pointVec, wallVec)
-			u = np.cross(wallPos - pointPos, pointVec) / np.cross(pointVec, wallVec)
-			
-			numIntersections += t > 0 and t < 1 and u > 0 and u < 1
+            t = np.cross(wallPos - pointPos, wallVec) / np.cross(pointVec, wallVec)
+            u = np.cross(wallPos - pointPos, pointVec) / np.cross(pointVec, wallVec)
+            
+            numIntersections += t > 0 and t < 1 and u > 0 and u < 1
 
-		if numIntersections % 2 :
-			return True
-	return False
-			
-	
+        if numIntersections % 2 :
+            return True
+    return False
+            
+    
 
 def iterate(plt, width, timestep, position, velocity, wallPos, wallVec, wallNorm, path) :
 
-	# Collisions
-	w = np.tile(wallPos.reshape(1,len(wallPos),2), (len(position),1,1))
-	wv = np.tile(wallVec.reshape(1,len(wallVec),2), (len(position),1,1))
+    # Collisions
+    w = np.tile(wallPos.reshape(1,len(wallPos),2), (len(position),1,1))
+    wv = np.tile(wallVec.reshape(1,len(wallVec),2), (len(position),1,1))
 
-	e = np.tile(position, (1,len(wallPos),1))
-	ev = np.tile(velocity*timestep, (1,len(wallPos),1))
-	
-	t = np.cross(w - e, wv) / np.cross(ev, wv)
-	u = np.cross(w - e, ev) / np.cross(ev, wv)
+    e = np.tile(position, (1,len(wallPos),1))
+    ev = np.tile(velocity*timestep, (1,len(wallPos),1))
+    
+    t = np.cross(w - e, wv) / np.cross(ev, wv)
+    u = np.cross(w - e, ev) / np.cross(ev, wv)
 
-	mask = (t > 0) * (t < 1) * (u > 0) * (u < 1)
+    mask = (t > 0) * (t < 1) * (u > 0) * (u < 1)
 
-	wvn = np.tile(wallNorm.reshape(1,len(wallNorm),2), (len(velocity),1,1))
+    wvn = np.tile(wallNorm.reshape(1,len(wallNorm),2), (len(velocity),1,1))
 
-	if(np.sum(mask) >= 1) :	
-		reflections = ev / timestep - 2 * wvn * np.inner(velocity, wallNorm).reshape((len(velocity),len(wallNorm),1))
-		maskedRefl = mask.reshape((len(velocity),len(wallNorm),1)) * reflections
-		compressedRefl = np.sum(maskedRefl, axis=1,).reshape(len(velocity),1,2)
-		invMask = np.sum(mask, axis=1,).reshape(len(velocity),1) < 1
-		velocity[:,0] = invMask*velocity[:,0] + compressedRefl[:,0]
+    if(np.sum(mask) >= 1) : 
+        reflections = ev / timestep - 2 * wvn * np.inner(velocity, wallNorm).reshape((len(velocity),len(wallNorm),1))
+        maskedRefl = mask.reshape((len(velocity),len(wallNorm),1)) * reflections
+        compressedRefl = np.sum(maskedRefl, axis=1,).reshape(len(velocity),1,2)
+        invMask = np.sum(mask, axis=1,).reshape(len(velocity),1) < 1
+        velocity[:,0] = invMask*velocity[:,0] + compressedRefl[:,0]
 
 
-	position += velocity*timestep
+    position += velocity*timestep
 
-	# Right periodic boundry
-	rightMask = position[:,:,0] > width
-	position[:,:,0] = (1-rightMask) * position[:,:,0] + rightMask * (position[:,:,0] - width)
-	# Left periodic boundry
-	leftMask = position[:,:,0] < 0
-	position[:,:,0] = (1-leftMask) * position[:,:,0] + leftMask * (width + position[:,:,0])
+    # Right periodic boundry
+    rightMask = position[:,:,0] > width
+    position[:,:,0] = (1-rightMask) * position[:,:,0] + rightMask * (position[:,:,0] - width)
+    # Left periodic boundry
+    leftMask = position[:,:,0] < 0
+    position[:,:,0] = (1-leftMask) * position[:,:,0] + leftMask * (width + position[:,:,0])
 
-	for i in range(0,min(len(position),MAX_PLOT_PATHS)) :
-		if ~leftMask[i] and ~rightMask[i]:
-			path[i][0].append(position[i][0][0])
-			path[i][1].append(position[i][0][1])
-		else :
-			path[i][0].append(np.nan)
-			path[i][1].append(np.nan)
+    for i in range(0,min(len(position),MAX_PLOT_PATHS)) :
+        if ~leftMask[i] and ~rightMask[i]:
+            path[i][0].append(position[i][0][0])
+            path[i][1].append(position[i][0][1])
+        else :
+            path[i][0].append(np.nan)
+            path[i][1].append(np.nan)
 
 def scatter(vtherm, velocities, timestep, meanTime) :
-	scatterProb = 1 - np.exp(-1*timestep/meanTime)
-	for velocity in velocities :
-		if scatterProb > np.random.uniform(0,1) :
-			angle = np.random.uniform(0,2*np.pi)
-			velocity[0] = [vtherm*np.cos(angle),
-				vtherm*np.sin(angle)]
+    scatterProb = 1 - np.exp(-1*timestep/meanTime)
+    for velocity in velocities :
+        if scatterProb > np.random.uniform(0,1) :
+            angle = np.random.uniform(0,2*np.pi)
+            velocity[0] = [vtherm*np.cos(angle),
+                vtherm*np.sin(angle)]
 
 def draw(plt, width, height, paths, edges) :
-	ax = plt.gca()
-	ax.set_xlim([0-width*0.5,width*1.5])
-	ax.set_ylim([0-height*0.5,height*1.5])
+    ax = plt.gca()
+    ax.set_xlim([0-width*0.5,width*1.5])
+    ax.set_ylim([0-height*0.5,height*1.5])
 
-	for path in paths :
-		plt.plot(path[0], path[1])
-	for edge in edges :
-		#plt.arrow(edge[0], edge[1], edge[2], edge[3], head_width=2e-9, head_length=5e-9, width = 0.01e-9, fc='k', ec='k')
-		plt.arrow(edge[0], edge[1], edge[2], edge[3], head_width=0.0, head_length=0.0, width = 0.01e-9, fc='k', ec='k')
-	plt.show()
+    for path in paths :
+        plt.plot(path[0], path[1])
+    for edge in edges :
+        #plt.arrow(edge[0], edge[1], edge[2], edge[3], head_width=2e-9, head_length=5e-9, width = 0.01e-9, fc='k', ec='k')
+        plt.arrow(edge[0], edge[1], edge[2], edge[3], head_width=0.0, head_length=0.0, width = 0.01e-9, fc='k', ec='k')
+    plt.show()
 
 def heatMap(plt, width, height, positions) :
-	lastX = []
-	lastY = []
-	for position in positions :
-		# kinda a hack
-		if position[0][1] < height and position[0][1] > 0:
-			lastX.append(position[0][0])
-			lastY.append(position[0][1])
+    lastX = []
+    lastY = []
+    for position in positions :
+        # kinda a hack
+        if position[0][1] < height and position[0][1] > 0:
+            lastX.append(position[0][0])
+            lastY.append(position[0][1])
 
-	ax = plt.gca()
-	ax.set_xlim([0,width])
-	ax.set_ylim([0,height])
+    ax = plt.gca()
+    ax.set_xlim([0,width])
+    ax.set_ylim([0,height])
 
-	ax.hist2d(lastX,lastY,bins=20)
-	plt.show()
+    ax.hist2d(lastX,lastY,bins=20)
+    plt.show()
 
 def simulate(numElectrons, numIterations, world, scatterElectrons = True, showProgress = False) :
-	timestep = 2e-15
-	width = 200e-9
-	height = 100e-9
-	vtherm = 1.87e5
+    timestep = 2e-15
+    width = 200e-9
+    height = 100e-9
+    vtherm = 1.87e5
 
 
-	wallPos, wallVec, wallNorm, polys, edges = world(width,height)
+    wallPos, wallVec, wallNorm, polys, edges = world(width,height)
 
-	position, velocity, path = initElectrons(numElectrons, width, height, vtherm, polys)
-	for i in range(numIterations) :
-		if scatterElectrons :
-			scatter(vtherm, velocity, timestep, 1.2e-12)
-		iterate(plt, width, timestep,  position, velocity, wallPos, wallVec, wallNorm, path)
-		if showProgress :
-			print(i)
+    position, velocity, path = initElectrons(numElectrons, width, height, vtherm, polys)
+    for i in range(numIterations) :
+        if scatterElectrons :
+            scatter(vtherm, velocity, timestep, 1.2e-12)
+        iterate(plt, width, timestep,  position, velocity, wallPos, wallVec, wallNorm, path)
+        if showProgress :
+            print(i)
 
-	draw(plt, width, height, path, edges)
-	heatMap(plt, width, height, position)
+    draw(plt, width, height, path, edges)
+    heatMap(plt, width, height, position)
 
 if __name__ == "__main__" :
-	simulate(1000,1000,shapes.parabolicFocus,showProgress = True)
+    simulate(1000,1000,shapes.parabolicFocus,showProgress = True)
 
